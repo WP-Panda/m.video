0f79989feeaa05e9571fb3a47aaed4c2fa440745
Максим <yoowordpress@yandex.ru>
2015-10-16 06:13:11 +0300

    Обновленный слайдер

--- wp-content/themes/wppandashop5/templates/components/section/slider.php
+++ wp-content/themes/wppandashop5/templates/components/section/slider.php
@@ -3,8 +3,8 @@ global $wps5_option;
 $array = $wps5_option['home-slider'];
 ?>
 
-<div class="hero-style-2" id="hero">
-    <div class="big-slider owl-main owl-carousel owl-inner-nav owl-ui-lg" id="owl-main">
+<div class="furniture-inner hero-style-3" id="hero">
+    <div class="sliders owl-main owl-carousel owl-inner-nav owl-ui-lg" id="owl-main">
 
         <?php foreach( $array as $key ) { ?>
 
@@ -14,7 +14,7 @@ $array = $wps5_option['home-slider'];
                         <h1 class="fadeInDown-1"><?php echo $key['title']; ?></h1>
                         <h6 class="fadeInDown-2"><?php echo $key['description']; ?></h6>
                         <div class="slide-btn fadeInDown-3">
-                            <a href="#" class="btn btn-primary">Shop Now</a>
+                            <a href="<?php echo $key['url']; ?>" class="btn btn-primary">Shop Now</a>
                         </div>
                     </div>
                 </div>
